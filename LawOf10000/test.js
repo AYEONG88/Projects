83fe4d1a11079ba4f17dde5b709a11490ba5c7b7
hanayeong <110640378+AYEONG88@users.noreply.github.com>
2023-05-24 22:25:40 +0900

    8th revised

---
+++ LawOf10000/test.js
@@ -0,0 +1,12 @@
+const prop={
+    name:'Button',
+    styles:{
+        size:20, color:'black',
+    }
+};
+
+function changeColor({name, styles:{size,color}}){
+    console.log(color);
+}
+
+changeColor(prop);
